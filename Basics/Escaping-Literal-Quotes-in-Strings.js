557b27e77527405c30d997642fa9c3bd0e084b1e
Abdullah Alsabouni <98663047+abdullah012345678910@users.noreply.github.com>
2023-12-07 20:47:41 +0300

    Create Escaping-Literal-Quotes-in-Strings.js

---
+++ Basics/Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,2 @@
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"." ; // Change this line
+console.log(myStr);// \"content\" // content : Çıktıdatıda çift tırnaklar arasında görüntülenir  // => I am a "double quoted" string inside "double quotes".
