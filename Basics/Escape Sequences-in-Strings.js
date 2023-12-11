079843f4fad82a20b69d569b0334cf0a839f6a5a
Abdullah Alsabouni <98663047+abdullah012345678910@users.noreply.github.com>
2023-12-11 20:03:37 +0300

    Create Escape Sequences-in-Strings.js

---
+++ Basics/Escape Sequences-in-Strings.js
@@ -0,0 +1,5 @@
+const myStr = 'FirstLine\n\t\\SecondLine\nThirdLine'; 
+              // String'in içinde 2 tane yan çizge yazılırsa bir tanesi görüntülenir
+console.log(myStr); //FirstLine
+                    //	\SecondLine
+                    //ThirdLine
